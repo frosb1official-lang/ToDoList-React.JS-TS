69a0244b0aa8b1bea06ee4ec2f03dc64681ee742
Frosb1 <frosb1official@gmail.com>
2026-03-25 10:23:32 +0700

    feat: added toastify library

--- src/components/ToDoList/ToDoListItem/ToDoListItem.tsx
+++ src/components/ToDoList/ToDoListItem/ToDoListItem.tsx
@@ -1,18 +1,46 @@
 import { ToDo } from '../../../models/todo-item'
+import { Bounce, ToastContainer, toast } from 'react-toastify'
 import './ToDoListItem.scss'
 
 export const ToDoListItem = (props: { toDoItem: ToDo , updateToDo: Function, deleteToDo: Function }) => {
+    // const notify =() => toast("Wow so easy!")
+
     return (
         <li className="todo-list-item__wrapper">
             <span>{props.toDoItem.text}</span>
             <div className="todo-list-item__buttons">
                 <button 
                     className="btn-trash"
-                    onClick={() => props.deleteToDo(props.toDoItem)}
+                    onClick={() => {
+                        props.deleteToDo(props.toDoItem); 
+                        toast.success(`${props.toDoItem.text} deleted!`, {
+                            position: "top-right",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: false,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "light",
+                            transition: Bounce,
+                            });
+                        }}
                     ></button>
                 <button 
                     className={props.toDoItem.isDone ? 'btn-check' : 'btn-uncheck'}
-                    onClick={() => props.updateToDo(props.toDoItem)}
+                    onClick={() => {props.updateToDo(props.toDoItem);
+                        toast.success(`${props.toDoItem.text} update!`, {
+                            position: "top-right",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: false,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "light",
+                            transition: Bounce,
+                            });
+                    }}
                     ></button>
             </div>
         </li>
--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -3,6 +3,7 @@ import { Form } from "../components/Form/Form";
 import { Header } from "../components/Header/Header";
 import { ToDoList } from "../components/ToDoList/ToDoList";
 import { ToDo } from "../models/todo-item";
+import { Bounce, ToastContainer } from "react-toastify";
 
 export const ToDoListPage = () => {
     const [todos, setTodos] = useState<ToDo[]>([])
@@ -35,6 +36,19 @@ export const ToDoListPage = () => {
         <>
             <Header />
             <Form createNewToDo={createNewToDo}/>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition={Bounce}
+                />
             <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo}/>
         </>
     );
