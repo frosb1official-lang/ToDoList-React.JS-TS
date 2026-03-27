358a6019c241c41297945cc3466b5c966021f93e
Frosb1 <frosb1official@gmail.com>
2026-03-27 18:35:11 +0700

    refactoring routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,63 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ToDoListPage } from './pages/ToDoListPage';
 import './Assets/scss/normalize.scss';
 import './Assets/scss/style.scss';
-import { HomePage } from './pages/HomePage';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { ToDo } from './models/todo-item';
-import { NotFound } from './pages/404';
-import { ItemDescription } from './pages/ItemDescription';
-import { Layout } from './layouts/Layout';
-
-const todos: ToDo[] = [
-  {
-    id: 0,
-    text: 'Первое действие',
-    isDone: false
-  },
-  {
-    id: 1,
-    text: 'Второе действие',
-    isDone: true
-  },
-  {
-    id: 2,
-    text: 'Третье действие',
-    isDone: false
-  },
-  {
-    id: 3,
-    text: 'Четвертое действие',
-    isDone: true
-  }
-]
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    errorElement: <NotFound />,
-    children : [
-      {
-        path: '/',
-        element: <HomePage todos={todos}/>
-      },
-      {
-        path: '/todo',
-        element: <ToDoListPage />
-      },
-      {
-        path: '/list/:id',
-        element: <ItemDescription todos={todos}/>
-      }
-    ]
-  },
-  {
-    path: '*',
-    element: <NotFound />
-  }
-], { basename: '/app/' })
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLInputElement
